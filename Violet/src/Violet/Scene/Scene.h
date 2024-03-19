fd5387f9c76ccc83fab0517a391d0da2f013f1a7
Sakana <2624712382@qq.com>
2024-03-19 15:24:27 +0800

    New UI to Add/Remove Components during Runtime

--- Violet/src/Violet/Scene/Scene.h
+++ Violet/src/Violet/Scene/Scene.h
@@ -11,10 +11,15 @@ namespace Violet {
 		~Scene();
 
 		Entity CreateEntity(const std::string& name = std::string());
+		void DestroyEntity(Entity entity);
 		inline entt::registry& Reg() { return m_Registry; }
 
 		void OnUpdate(Timestep timestep);
 		void OnViewportResize(uint32_t m_Width, uint32_t m_Height);
+	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
+		// void OnComponentAdded(Entity entity, CameraComponent& component);
 	private:
 		entt::registry m_Registry;
 		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
